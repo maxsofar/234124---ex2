caf55973b15ec9c117bd6643a8c9cdd89ad2f08a
Max <spiiked@gmail.com>
2022-05-05 21:48:15 +0300

    Card methods implementation

---
+++ Card.cpp
@@ -0,0 +1,55 @@
+#include "Card.h"
+
+Card::Card(CardType type, const CardStats& stats) {
+    m_effect = type;
+    m_stats = stats;
+}
+
+void Card::applyEncounter(Player &player) const {
+    switch (m_effect) {
+        case CardType::Battle:
+            if (player.getAttackStrength() >= m_stats.force) {
+                player.levelUp();
+                player.addCoins(m_stats.loot);
+            } else {
+                player.damage(m_stats.hpLossOnDefeat);
+            }
+            break;
+
+        case CardType::Buff:
+                if (player.pay(m_stats.cost)) {
+                    player.buff(m_stats.buff);
+                }
+            break;
+
+        case CardType::Heal:
+            if (player.pay(m_stats.cost)) {
+                player.heal(m_stats.heal); //if HP maxed out should he pay?
+            }
+            break;
+
+        case CardType::Treasure:
+            player.addCoins(m_stats.loot);
+            break;
+    }
+}
+
+void Card::printInfo() const {
+    switch (m_effect) {
+        case CardType::Battle:
+            printBattleCardInfo(m_stats);
+            break;
+
+        case CardType::Buff:
+            printBuffCardInfo(m_stats);
+            break;
+
+        case CardType::Heal:
+            printHealCardInfo(m_stats);
+            break;
+
+        case CardType::Treasure:
+            printTreasureCardInfo(m_stats);
+            break;
+    }
+}
