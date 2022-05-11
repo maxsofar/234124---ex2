f8f5a46a8e424677e5323f61481356e0ccde3cdc
Max <spiiked@gmail.com>
2022-05-11 20:19:03 +0300

    Mtmchkin implementation

---
+++ Mtmchkin.cpp
@@ -0,0 +1,35 @@
+#include "Mtmchkin.h"
+
+Mtmchkin::Mtmchkin(const char *playerName, const Card *cardsArray, int numOfCards) : m_player(Player(playerName)) {
+    m_gameStatus = GameStatus::MidGame;
+    m_numOfCards = numOfCards;
+    m_cardIndex = 0;
+    m_cardStack = new Card(*cardsArray);
+}
+
+
+void Mtmchkin::playNextCard() {
+    m_cardStack[m_cardIndex].printInfo();
+    m_cardStack[m_cardIndex].applyEncounter(m_player);
+    m_player.printInfo();
+    ++m_cardIndex;
+    if (m_cardIndex == m_numOfCards) {
+        m_cardIndex = 0;
+    }
+}
+
+GameStatus Mtmchkin::getGameStatus() const {
+    return m_gameStatus;
+}
+
+bool Mtmchkin::isOver() {
+    if (m_player.isKnockedOut()) {
+        m_gameStatus = GameStatus::Loss;
+        return true;
+    } else if (m_player.getLevel() == 10) {
+        m_gameStatus = GameStatus::Win;
+        return true;
+    } else {
+        return false;
+    }
+}
